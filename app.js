f33e072bc2747142a87244073a833ef96fb190ac
mbizin-v <murilo.bizin@venturus.org.br>
2024-04-26 17:57:05 -0300

    Introdução módulos nativos http e fs

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('Olá, mundo!');
+import http from 'http';
+import fs from 'fs';
+
+fs.writeFile('./mensagem.txt', 'Olá, TIC em Trilhas do arquivo!', 'utf-8', (erro) => {
+    if (erro) {
+        console.log('Falha ao escrever o arquivo', erro);
+        return;
+    }
+    console.log('Arquivo foi criado com sucesso');
+});
+
+fs.readFile('./mensagem.txt', 'utf-8', (erro, conteudo) => {
+    if (erro) {
+        console.log('Falha na leitura do arquivo', erro);
+        return;
+    }
+
+    console.log(`Conteúdo: ${conteudo}`);
+
+    iniciaServidorHttp(conteudo);
+});
+
+function iniciaServidorHttp(mensagem){
+    const servidor = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/plain; charset=utf-8');
+        res.end(mensagem);
+    });
+
+    const porta = 3000;
+    const host = 'localhost';
+
+    servidor.listen(porta, host, () => {
+        console.log(`Servidor executando em http://${host}:${porta}/`)
+    });
+}
